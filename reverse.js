38524167ddf3dc49eae7046bc399c04a2ec40c18
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-07-07 11:46:49 +0600

    Apply built in method to make reverse a string

--- reverse.js
+++ reverse.js
@@ -1,17 +1,22 @@
+// reverse a string without bult in function
 
-function reverseString(str){
-    var reverse = "";
-    for (i = 0; i<str.length; i++){
-        var char = str[i];
-        reverse = char + reverse;
-    }
-    return reverse;
+function reverseString(str) {
+	var reverse = '';
+	for (i = 0; i < str.length; i++) {
+		var char = str[i];
+		reverse = char + reverse;
+	}
+	return reverse;
 }
 
-var statement = "Hello Alien bhai brother"
+var statement = 'Hello Bangladeshi!';
 
 var forAlien = reverseString(statement);
 console.log(forAlien);
 
+// reverse a string with built in function
+function reverseStr(str) {
+	return str.split('').reverse().join('');
+}
 
-
+console.log(reverseStr(statement));
